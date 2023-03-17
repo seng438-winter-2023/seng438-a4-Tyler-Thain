86e2a42ccace57b9fbef5e2bb4d1f22fbf7825ba
JaredAssen <80238577+JaredAssen@users.noreply.github.com>
2023-03-17 08:04:49 -0600

    utilities mutation tests

---
+++ JFreeChart_Lab4/src/org/jfree/data/datautilitiestest/CalculateRowTotalTest.java
@@ -0,0 +1,60 @@
+package org.jfree.data.datautilitiestest;
+
+import static org.junit.Assert.*;
+
+import java.security.InvalidParameterException;
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.DefaultKeyedValues2D;
+import org.jfree.data.Values2D;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.*;
+
+public class CalculateRowTotalTest extends DataUtilities{
+	
+	@Test
+	public void validInputTest() {
+		// This test covers a valid input for data and row (ECT)
+	
+	    //Mock does not work with EclEmma so test written with actual dependency 
+	    DefaultKeyedValues2D values = new DefaultKeyedValues2D();
+	    values.setValue(-2.5, 0, 0);
+	    values.setValue(null, 0, 1);
+	    values.setValue(7.5, 0, 2);
+	    double result = DataUtilities.calculateRowTotal(values, 0);
+		assertEquals("Total should be 10 for valid input for index 0", 5, result, .000000001d);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void nullDataTest() {
+		// This test covers an invalid input for data and a valid input for row (ECT)
+	    double actual = DataUtilities.calculateRowTotal(null, 1);
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void negativeIndexTest() {
+		// This test covers an invalid input for row and a valid input for data (BVT)
+		
+		//Mock does not work with EclEmma so test written with actual dependency 
+	    DefaultKeyedValues2D values = new DefaultKeyedValues2D();
+	    values.setValue(2.5, 0, 0);
+	    values.setValue(7.5, 0, 1);
+	    
+		double result = DataUtilities.calculateRowTotal(values, -1);
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void tooLargeIndexTest() {
+		// This test covers an invalid input for row and a valid input for data (BVT)
+
+		//Mock does not work with EclEmma so test written with actual dependency 
+	    DefaultKeyedValues2D values = new DefaultKeyedValues2D();
+	    values.setValue(2.5, 0, 0);
+	    values.setValue(7.5, 0, 1);
+		double result = DataUtilities.calculateRowTotal(values, 10);
+
+	}
+	
+
+}
---
+++ JFreeChart_Lab4/src/org/jfree/data/datautilitiestest/GetCumulativePercentageTest.java
@@ -0,0 +1,79 @@
+package org.jfree.data.datautilitiestest;
+
+import static org.junit.Assert.*;
+
+import java.security.InvalidParameterException;
+import org.junit.*;
+import org.jfree.data.DataUtilities;
+import org.jfree.data.DefaultKeyedValues;
+import org.jfree.data.KeyedValues;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+
+public class GetCumulativePercentageTest extends DataUtilities {
+	Mockery mockingContext;
+	KeyedValues values;
+	
+	@BeforeClass public static void setUpBeforeClass() throws Exception {
+    }
+
+
+    @Before
+    public void setUp() throws Exception {
+    	
+    }
+	
+	// This test covers when data is null
+	@Test(expected = IllegalArgumentException.class)
+	public void nulldataTest() throws IllegalArgumentException{
+		DataUtilities.getCumulativePercentages(null);
+	}
+	
+	// This test covers for valid data
+	// Mock does not work with EclEmma so test written with actual dependency
+	@Test
+	public void validDataObject() {
+		DefaultKeyedValues values = new DefaultKeyedValues();
+		double nums[] = {2.0, 2.0, 2.0, 2.0, 2.0};
+		values.setValue(new Double(0), nums[0]);
+		values.setValue(new Double(1), nums[1]);
+		values.setValue(new Double(2), nums[2]);
+		values.setValue(new Double(3), nums[3]);
+		values.setValue(new Double(4), nums[4]);
+		
+	    KeyedValues actual = getCumulativePercentages(values);
+		double expected[] = {0.2, 0.4, 0.6, 0.8, 1.0};
+		for(int i = 0; i < 5; i++) {
+			assertEquals(expected[i], actual.getValue(i));
+		}
+	}
+	
+	// This test covers the case of a value in a key-value pair being null
+	// Mock does not work with EclEmma so test written with actual dependency
+	@Test
+	public void nullValueInKeyedValuesTest() {
+		DefaultKeyedValues values = new DefaultKeyedValues();
+		double nums[] = {0.0, 1.0, 2.0, 3.0, 4.0};
+		values.setValue(new Double(0), null);
+		values.setValue(new Double(1), nums[1]);
+		values.setValue(new Double(2), nums[2]);
+		values.setValue(new Double(3), nums[3]);
+		values.setValue(new Double(4), nums[4]);
+		KeyedValues actual = getCumulativePercentages(values);
+		double expected[] = {0.0, 0.1, 0.3, 0.6, 1.0};
+		for(int i = 0; i < 5; i++) {
+			assertEquals(expected[i], actual.getValue(i));
+		}
+	}
+	
+	
+	@After
+    public void tearDown() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+
+}
